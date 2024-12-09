216e69104ee0b70d214f36204ee928c38ae074ca
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-12-09 19:44:05 -0300

    Add configurable $SERVER variable to clone script
    To make the script more flexible, the $SERVER variable was added. Now, you can easily change the server IP address by modifying the $SERVER value in the script. :)

--- clone-repo.sh
+++ clone-repo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <repository-name>"
+    echo "Usage: $0 <repository-name> [server] [repo-path]"
     exit 1
 fi
 
 REPO_NAME=$1
-git clone "server:~/repos/bare/${REPO_NAME}.git"
+SERVER="ssh://192.168.1.100"
+
+git clone "$SERVER:~/repos/bare/$REPO_NAME.git"
