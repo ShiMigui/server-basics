2b139631020efa54df0a66315bfb71e1c752b099
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-12-12 13:28:59 -0300

    Add $@ to mirror and bare

--- tools/bare.sh
+++ tools/bare.sh
@@ -7,7 +7,7 @@ case $FEATURE in
     execute_command "cd $BARE_DIR/$REPO_NAME && git push $@"
     ;;
 "clone")
-    execute_command "cd $BARE_DIR && git clone --bare $REPO_URL"
+    execute_command "cd $BARE_DIR && git clone --bare $REPO_URL $@"
     ;;
 *)
     execute_command "cd $BARE_DIR && git $@"
--- tools/mirror.sh
+++ tools/mirror.sh
@@ -4,11 +4,11 @@ source "./tools/_repositories_init.sh"
 
 case $FEATURE in
 "create")
-    execute_command "cd $MIRROR_DIR && git clone $BARE_DIR/$REPO_NAME"
+    execute_command "cd $MIRROR_DIR && git clone $BARE_DIR/$REPO_NAME $@"
     ;;
 "sync")
     origin=${@:-"origin main"}
-    execute_command "cd $MIRROR_DIR/$REPO_NAME && git pull $origin"
+    execute_command "cd $MIRROR_DIR/$REPO_NAME && git pull $origin $@"
     ;;
 *)
     echo "Unknown command: $FEATURE"
