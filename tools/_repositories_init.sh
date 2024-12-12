b1b52c5cd4358b332d62fca46acc955918c7d0fd
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-12-12 14:27:43 -0300

    --amend

--- tools/_repositories_init.sh
+++ tools/_repositories_init.sh
@@ -2,7 +2,7 @@
 
 REPO_URL=${1:-"<repository>"}
 
-usage 1 "_repositories_init: $0 $TOOL $FEATURE $REPO_URL [...args]" $@
+usage 1 "_repositories_init: $0 $HOST $TOOL $FEATURE $REPO_URL [...args]" $@
 shift 1
 
 REPO_NAME=$(basename "$REPO_URL")
