d94deff3d7acca60b134298815e142060a396fb2
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-12-12 16:11:31 -0300

    --amend

--- main.sh
+++ main.sh
@@ -3,13 +3,14 @@
 source "./source/config.sh"
 
 source "./func/execute.sh"
+source "./func/execute_in_wd.sh"
 source "./func/usage.sh"
 
 HOST=${1:-"<host>"}
 TOOL=${2:-"<tool>"}
 FEATURE=${3:-"<feature>"}
 
-usage 3 "server.sh: $HOST $TOOL $FEATURE ...args" $@
+usage 3 "server.sh: $HOST $TOOL $FEATURE" $@
 shift 3
 
 case $TOOL in
